30eb09ae96fe2095af52b21227d6a292a84d2e53
Jakob Jul Elben <elbenjakobjul@gmail.com>
2022-01-06 13:48:01 +0100

    More changes

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -1,49 +1,58 @@
 import sys
-from pathlib import Path
 
+import nox
 from nox_poetry import Session, session
 
-CODE_DIRECTORIES = ["src/{{cookiecutter.project_name}}", "tests"]
+CODE_DIRECTORIES = ["{{cookiecutter.project_slug}}", "tests"]
 CODE_DIRECTORIES_WITH_NOTEBOOKS = CODE_DIRECTORIES + ["notebooks"]
-PYTHON_VERSIONS = ("3.8",)
 DEFAULT_PYTHON_VERSION = False
-DEFAULT_SESSIONS = (
+DEFAULT_VENV_BACKEND = "none"
+MINIMAL_CODE_COVERAGE = 70
+
+# Specify default tasks
+nox.options.sessions = (
     "Check Code Format",
     "Import Checking",
     "Unit Tests",
     "Type Checking",
     "Lint Checking",
 )
-MINIMAL_CODE_COVERAGE = 70
 
 
 @session(
     name="Check Code Format",
     python=DEFAULT_PYTHON_VERSION,
+    venv_backend=DEFAULT_VENV_BACKEND,
 )
 def format_checking(session: Session) -> None:
-    session.install("black")
     session.run("black", "--check", *CODE_DIRECTORIES_WITH_NOTEBOOKS)
 
 
-@session(name="Import Checking", python=DEFAULT_PYTHON_VERSION)
+@session(
+    name="Import Checking",
+    python=DEFAULT_PYTHON_VERSION,
+    venv_backend=DEFAULT_VENV_BACKEND,
+)
 def import_checking(session: Session) -> None:
-    session.install("isort")
     session.run("isort", "--check-only", *CODE_DIRECTORIES)
 
 
-@session(name="Lint Checking", python=DEFAULT_PYTHON_VERSION)
+@session(
+    name="Lint Checking",
+    python=DEFAULT_PYTHON_VERSION,
+    venv_backend=DEFAULT_VENV_BACKEND,
+)
 def import_checking(session: Session) -> None:
-    session.install("flake8")
     session.run("flake8", *CODE_DIRECTORIES)
 
 
-@session(name="Unit Tests", python=PYTHON_VERSIONS)
+@session(
+    name="Unit Tests", python=DEFAULT_PYTHON_VERSION, venv_backend=DEFAULT_VENV_BACKEND
+)
 def unit_test(session: Session) -> None:
-    session.run_always("poetry", "install", external=True)
     session.run(
         "pytest",
-        "--cov=src/{{cookiecutter.project_name}}",
+        "--cov={{cookiecutter.project_slug}}",
         f"--cov-fail-under={MINIMAL_CODE_COVERAGE}",
         "-n",
         "auto",
@@ -51,20 +60,25 @@ def unit_test(session: Session) -> None:
     )
 
 
-@session(name="Type Checking", python=DEFAULT_PYTHON_VERSION)
+@session(
+    name="Type Checking",
+    python=DEFAULT_PYTHON_VERSION,
+    venv_backend=DEFAULT_VENV_BACKEND,
+)
 def type_checking(session: Session) -> None:
-    session.run_always("poetry", "install", external=True)
     args = session.posargs or CODE_DIRECTORIES
     session.run("mypy", f"--python-executable={sys.executable}", *args)
 
 
-@session(name="Build Docs", python=DEFAULT_PYTHON_VERSION)
+@session(
+    name="Build Docs", python=DEFAULT_PYTHON_VERSION, venv_backend=DEFAULT_VENV_BACKEND
+)
 def build_docs(session: Session) -> None:
-    session.install("mkdocs", "mkdocs-material")
     session.run("mkdocs", "build", *session.posargs)
 
 
-@session(name="Serve Docs", python=DEFAULT_PYTHON_VERSION)
+@session(
+    name="Serve Docs", python=DEFAULT_PYTHON_VERSION, venv_backend=DEFAULT_VENV_BACKEND
+)
 def serve_docs(session: Session) -> None:
-    session.install("mkdocs", "mkdocs-material")
     session.run("mkdocs", "serve", *session.posargs)
